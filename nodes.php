ba27f857cf339c121cbfd120cc1e58d9f4715912
@justyy <dr.zhihua.lai@gmail.com>
2019-02-02 12:41:11 +0000

    Create nodes.php

---
+++ nodes.php
@@ -0,0 +1,24 @@
+<input id='nodes' onclick='this.select()' style='max-width:100%' class='form-control' list="nodes_list" value="https://api.steemit.com" placeholder="https://api.steemit.com">
+<datalist id="nodes_list">
+  <option value='https://api.steemit.com'>
+  <option value='https://rpc.steemviz.com'>
+  <option value='https://rpc.buildteam.io'>
+  <option value='https://gtg.steem.house:8090'>
+  <option value='https://steemd.minnowsupportproject.org'>
+  <option value='https://rpc.steemliberator.com'>
+  <option value='https://seed.bitcoiner.me'>
+  <option value='https://steemd.pevo.science'>
+  <option value='https://steemd.privex.io'>
+  <option value='https://steemd.steemitdev.com'>
+  <option value='https://steemd.steemitstage.com'>
+  <option value='https://steemd.steemgigs.org'>
+  <option value='https://appbasetest.timcliff.com'>
+  <option value='https://rpc.curiesteem.com'>
+  <option value='https://api.mahdiyari.info'>
+  <option value='https://api.steem.house'>
+  <option value='wss://rpc.steemviz.com'>
+  <option value='wss://rpc.buildteam.io'>
+  <option value='wss://gtg.steem.house:8090'>
+  <option value='wss://steemd.minnowsupportproject.org'>
+  <option value='wss://steemd.pevo.science'>    
+</datalist>
